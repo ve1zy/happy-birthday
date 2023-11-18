453fb14ac200eebed565915f1d93774fdc08299d
ve1zy <111706869+ve1zy@users.noreply.github.com>
2023-11-19 01:40:33 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function k() {
+  document.querySelector("#wr").innerHTML = "";
+  document.querySelector("#wr").innerHTML = `<div class="jop">
+  <h1>с днем рождения папа
+  желаю всего самого наилучшего!!!</h1>
+  <div class='pop'>  <img src="happy-birthday.gif" />
+  <img src="happy-birthday-birthday-cake.gif" />
+  <img src="birthday-cake.gif" /></div>
+</div>`;
+}
